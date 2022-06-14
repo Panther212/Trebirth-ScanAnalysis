3fcc2804111682121e1e481a7e34d6888139a19d
Mukund666 <mukund.choudhary14@gmail.com>
2022-06-14 22:20:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,24 +18,6 @@ import boto3
 import json
 
 
-# s3_client = boto3.client('s3')
-# response=s3_client.get_object(Bucket='trebirth1',Key='FB.json')
-# result = response['Body'].read()
-# print("Result is",result)
-#retrieve json file from firebase
-firebase = firebase.FirebaseApplication('https://esp32-d544d-default-rtdb.firebaseio.com/',None)
-result = firebase.get("test","sensor")
-result1 = firebase.get("test","scan_number")
-Np_result = np.array(result)
-df = pd.DataFrame(Np_result)
-df.to_csv( "scan.csv")
-
-
-
-jtopy=json.dumps(result)       #json.dumps take a dictionary as input and returns a string as output.
-dict_json=json.loads(jtopy)    # json.loads take a string as input and returns a dictionary as output.
-# print(dict_json)
-
 # Digital Filter starts from here
 def Data_Preprocess(x):
  sig = [np.array(x)]
@@ -142,11 +124,6 @@ def Calculate_FFT(sig_data):
    
    
 
-Data = Data_Preprocess(dict_json)
-# print("Data is ",Data)
-Filtered_data = Apply_Filter(Data)
-plt.savefig("output.jpg")
-
 
 
 #Streamlit GUI starts from here
@@ -162,56 +139,6 @@ a=st.sidebar.radio('Navigation',['Farm Information','Farmer Data'])
 
 if a == "Farm Information":
  st.header("Welcome to Trebirth Tech Development")
-#  form = st.form(key='my_form',clear_on_submit=True)
-#  F_name= form.text_input(label='Enter Farmer Name')
-#  F_health= form.text_input(label='Enter Farm Health')
-#  Number= form.number_input(label='Enter No. of trees scanned')
-#  Remark = form.text_area(label='Remark')
-#  submit_button = form.form_submit_button(label='Submit')
-
-
-#  st.sidebar.markdown(
-#     f"""
-#      * Farmer name :        {F_name}
-#      * Farm health :        {F_health}
-#      * No of trees scanned: {Number}
-#      * Remark      :        {Remark}
-#  """
-#   )
- st.subheader(f'Scan number is: {result1}')
- #st.write("Scan number is ", result1)
- Plot_Graph(Filtered_data)	
- Calculate_FFT(Np_result)
- Calculate_DCT(Np_result)
- Calculate_DST(Np_result)
- Calculate_STFT2(Np_result)
- Calculate_Phase_Spectrum(Np_result)	
- #st.line_chart(Filtered_data, width=1000, height=0, use_container_width=False)
- st.write(df)
- 
-
-
-#  if submit_button:
-#      st.write(F_name,F_health,Number,Remark)
-#  new_data = {"Farmer_Name": F_name,"Farm_Health": F_health,"Trees_Scanned": int(Number),"Remark": Remark}
-#  #st.write(new_data)
-#  df = df._append(new_data,ignore_index=True)
-#  df.to_csv("Trebirth.csv",index=False)
-# st.dataframe(df)
-
-
-@st.cache
-def convert_df(df):
- return df.to_csv().encode('utf-8')
-csv = convert_df(df)
-
-st.download_button(
-     "Press to Download",
-     csv,
-     "file.csv",
-     "text/csv",
-     key='download-csv'
- )
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:
